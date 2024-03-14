13428e17ce5041387f13183a1d29814dacdf4df6
Service Web <sw@gssmail.com>
2024-03-14 10:09:08 -0500

    Commit changes in C:\Websites\Git\emarlow\5425\GCG_5425_VALID_CODES.sql

---
+++ GCG_5425_VALID_CODES.sql
@@ -0,0 +1,10 @@
+/* ==========================================
+ * TABLE: GCG_5425_VALID_CODES
+ * ========================================== */
+CREATE TABLE "GCG_5425_VALID_CODES"(
+ "Country" CHAR(50),
+ "Code" CHAR(2),
+ "Last_Added" DATETIME,
+ "Added_By" CHAR(50));
+
+
